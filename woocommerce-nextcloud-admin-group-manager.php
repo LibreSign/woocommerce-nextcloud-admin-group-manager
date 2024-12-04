de69e9e4f053b5f43d972d51ae99531a3ab8114a
Vitor Mattos <vitor@php.rio>
2024-12-04 10:30:25 -0300

    chore: change plugin name
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- woocommerce-nextcloud-admin-group-manager.php
+++ woocommerce-nextcloud-admin-group-manager.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * Woo Order Complete Messagec
+ * WooCommerce Nextcloud Admin Group Manager
  *
  * @package   wp-simple-smtp
- * @author    Vitor Mattos <vitor@php.rio>
+ * @author    LibreCode <contact@librecode.coop>
  * @license   GPL-2.0+
  * @link      http://github.com/vitormattos
- * @copyright 2021 Vitor Mattos
+ * @copyright 2021 LibreCode
  *
  * @wordpress-plugin
- * Plugin Name:       Woo Order Complete Messagec
- * Plugin URI:        https://github.com/LibreSign/WoocommerceAPITrigger
- * Description:       Woo Order Complete Messagec
+ * Plugin Name:       WooCommerce Nextcloud Admin Group Manager
+ * Plugin URI:        https://github.com/LibreSign/woocommerce-nextcloud-admin-group-manager
+ * Description:       Integrate WooCommerce with LibreSign SaaS using the Nextcloud app Admin Group Manager
  * Version:           1.0.1
- * Author:            Vitor Mattos
+ * Author:            LibreCode
  * Author URI:        https://github.com/LibreSign
  * Text Domain:       wp-simple-smtp
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: https://github.com/LibreSign/WoocommerceAPITrigger
+ * GitHub Plugin URI: https://github.com/LibreSign/woocommerce-nextcloud-admin-group-manager
  */
 
 defined( 'ABSPATH' ) || exit;
