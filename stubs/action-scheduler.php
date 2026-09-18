db928d3f4fd0c764a0ac9a8168a19a5ca7177aed
YvesCesar <yvesamorim73@gmail.com>
2026-09-18 18:17:45 -0400

    chore: extend the coding standard and the static analysis to the test suite
    Signed-off-by: YvesCesar <yvesamorim73@gmail.com>

---
+++ stubs/action-scheduler.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @param string             $hook
+ * @param array<mixed>|null  $args
+ * @param string             $group
+ * @return int|bool
+ */
+function as_next_scheduled_action( $hook, $args = null, $group = '' ) {}
